766b19152456e07b72e77618a3c0d8dc41840c73
Nitish Jaiswal <141999440+nitishjaiswal2002@users.noreply.github.com>
2024-05-27 15:45:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,53 @@
 //your JS code here. If required.
+const circles = document.querySelectorAll('.circle');
+const prevButton = document.getElementById('prev');
+const nextButton = document.getElementById('next');
+let currentActive = 1;
+
+updateButtons();
+
+nextButton.addEventListener('click', () => {
+  currentActive++;
+
+  if (currentActive > circles.length) {
+    currentActive = circles.length;
+  }
+
+  update();
+});
+
+prevButton.addEventListener('click', () => {
+  currentActive--;
+
+  if (currentActive < 1) {
+    currentActive = 1;
+  }
+
+  update();
+});
+
+function update() {
+  circles.forEach((circle, idx) => {
+    if (idx < currentActive) {
+      circle.classList.add('active');
+    } else {
+      circle.classList.remove('active');
+    }
+  });
+
+  updateButtons();
+}
+
+function updateButtons() {
+  if (currentActive === 1) {
+    prevButton.disabled = true;
+  } else {
+    prevButton.disabled = false;
+  }
+
+  if (currentActive === circles.length) {
+    nextButton.disabled = true;
+  } else {
+    nextButton.disabled = false;
+  }
+}
